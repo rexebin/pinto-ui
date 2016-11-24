33047e9c3f5ccebea878450be2b9f14dac983312
Rex YE <rexebin@gmail.com>
2016-11-24 09:39:40 +0000

    update to angular-cli beta 21.

--- src/app/layout/top-nav-sub/top-nav-sub.component.spec.ts
+++ src/app/layout/top-nav-sub/top-nav-sub.component.spec.ts
@@ -10,13 +10,14 @@ const menuItemWithChildren: MenuItem = {
   label: 'item1',
   items: [
     {
-      label: 'sub-item1'
+      label: 'sub-item1',
+      icon:''
     },
     { label: 'sub-item2' }
   ]
 };
 
-const menuItemWithoutChildren:MenuItem  = {
+const menuItemWithoutChildren: MenuItem = {
   label: 'item1'
 };
 
@@ -42,36 +43,47 @@ describe('TopNavSubComponent', () => {
     expect(component.menuItem).toEqual({});
   });
 
-  it('render a single link with menuItem\'s label when menuItem has no items', async(()=>{
-    component.menuItem = menuItemWithoutChildren;
-    fixture.detectChanges();
+  describe('Button and dropdown rendering', () => {
 
-    const el = fixture.nativeElement.querySelectorAll('a');
+    it('should render a single link with menuItem\'s label when menuItem has no items', async(() => {
+      component.menuItem = menuItemWithoutChildren;
+      fixture.detectChanges();
 
-    expect(el.length).toBe(1);
+      const el = fixture.nativeElement.querySelectorAll('a');
 
-    expect(el[0].textContent).toBe(menuItemWithoutChildren.label);
+      expect(el.length).toBe(1);
 
-  }));
+      expect(el[0].textContent).toBe(menuItemWithoutChildren.label);
 
-  it('render a dropdown when menuItem has items and display labels correctly', async(()=>{
-    component.menuItem = menuItemWithChildren;
-    fixture.detectChanges();
-    const el = fixture.nativeElement.querySelector('div[ngbDropdown');
-    //dropdown is rendered.
-    expect(el).toBeTruthy();
-    // top level nav label.
-    const toggleEl = fixture.nativeElement.querySelector('a[ngbdropdowntoggle]');
-    expect(toggleEl.textContent).toBe(menuItemWithChildren.label);
-    // dropdown items's labels.
-    const dropDownMenuEl = fixture.nativeElement.querySelector('div.dropdown-menu');
-    const subMenuEls = dropDownMenuEl.querySelectorAll('a');
+    }));
 
-    subMenuEls.forEach((submenu, i) => {
-      expect(submenu.textContent).toBe(menuItemWithChildren.items[i].label);
+    it('should render a dropdown when menuItem has items and display labels correctly', async(() => {
+      component.menuItem = menuItemWithChildren;
+      fixture.detectChanges();
+      const el = fixture.nativeElement.querySelector('div[ngbDropdown');
+      //dropdown is rendered.
+      expect(el).toBeTruthy();
+      // top level nav label.
+      const toggleEl = fixture.nativeElement.querySelector('a[ngbdropdowntoggle]');
+      expect(toggleEl.textContent).toBe(menuItemWithChildren.label);
+      // dropdown items's labels.
+      const dropDownMenuEl = fixture.nativeElement.querySelector('div.dropdown-menu');
+      const subMenuEls = dropDownMenuEl.querySelectorAll('a');
+
+      subMenuEls.forEach((submenu, i) => {
+        expect(submenu.textContent).toBe(menuItemWithChildren.items[i].label);
+      });
+    }));
+
+  });
+
+  describe('MenuItem Icons', () => {
+
+    it('should render icon if there is a icon provided', ()=>{
+
     });
 
 
-  }));
 
+  });
 });
