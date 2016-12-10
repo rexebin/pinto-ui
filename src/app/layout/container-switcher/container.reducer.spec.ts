a504d974770941e8a17a8423fb80d6deb3c44592
Rex YE <rexebin@gmail.com>
2016-12-10 15:08:12 +0000

    container.action spec.

--- src/app/layout/container-switcher/container.reducer.spec.ts
+++ src/app/layout/container-switcher/container.reducer.spec.ts
@@ -1,24 +1,26 @@
-
 import { containerReducer } from './container.reducer';
 import { ContainerActions } from './container.actions';
 fdescribe('Container Reducer', () => {
+  
   let containerActions: ContainerActions;
+  
   beforeEach(() => {
     containerActions = new ContainerActions();
   });
   
   it('should return initial state', () => {
-    expect(containerReducer(undefined, {type: ''})).toEqual(false);
+    expect(containerReducer(undefined, { type: '' })).toEqual(false);
   });
+  
   it('should return true/false when action type is containerFluid/container', () => {
     expect(containerReducer(null, containerActions.setContainerFluid())).toEqual(true);
     expect(containerReducer(null, containerActions.setContainer())).toEqual(false);
   });
-
+  
   it('should return current state if type is not valid', () => {
-    expect(containerReducer(false, { type: 'nothingMatching' })).toEqual(false);
-    expect(containerReducer(true, { type: 'nothingMatching' })).toEqual(true);
-    expect(containerReducer(undefined, {type: 'nothingMatching'})).toEqual(false);
+    expect(containerReducer(false, { type: '' })).toEqual(false);
+    expect(containerReducer(true, { type: '' })).toEqual(true);
+    expect(containerReducer(undefined, { type: '' })).toEqual(false);
   });
-
+  
 });
