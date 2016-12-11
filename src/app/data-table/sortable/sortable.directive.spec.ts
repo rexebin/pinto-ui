667a45c0895c0a80128130af75474165ee26ee87
Rex YE <rexebin@gmail.com>
2016-12-11 13:28:06 +0000

    sortable service

--- src/app/data-table/sortable/sortable.directive.spec.ts
+++ src/app/data-table/sortable/sortable.directive.spec.ts
@@ -2,15 +2,25 @@
 
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { SortableDirective } from './sortable.directive';
-import { Component } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TableFilterService, SortParams } from '../table-filter.service';
 
-describe('SortableDirective', () => {
+fdescribe('SortableDirective', () => {
   let fixture: ComponentFixture<TestComponent>;
   let component: TestComponent;
+  let debugElement: DebugElement;
+  let sortParam: SortParams;
+  let sortable: SortableDirective;
+  let tableFilterService: TableFilterService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TestComponent, SortableDirective],
-      imports: []
+      declarations: [
+        TestComponent,
+        SortableDirective
+      ],
+      imports: [],
+      providers: [TableFilterService]
     })
       .compileComponents();
   }));
@@ -19,31 +29,59 @@ describe('SortableDirective', () => {
     fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement.query(By.directive(SortableDirective));
+    sortable = debugElement.injector.get(SortableDirective);
+    tableFilterService = TestBed.get(TableFilterService);
+    sortParam = {
+      entity: 'entity',
+      sortBy: 'property'
+    };
+    spyOn(sortable, 'onClick').and.callThrough();
+    spyOn(tableFilterService, 'filter');
+    
   });
-  it('should emit sortingOrPaging event with payload when clicking the host element', async(() => {
+  
+  it('should emit filter event with sortby and order when clicking the host element', () => {
+    let expectedParams = sortParam;
+    expectedParams.order = 'asc';
+    debugElement.nativeElement.click();
+    expect(sortable.onClick).toHaveBeenCalled();
+    expect(tableFilterService.filter).toHaveBeenCalledWith(expectedParams);
+    expectedParams.order = 'desc';
+    debugElement.nativeElement.click();
+    expect(sortable.onClick).toHaveBeenCalled();
+    expect(tableFilterService.filter).toHaveBeenCalledWith(expectedParams);
+  });
     
+  it('should show an upper arrow indicate asc order after emit event with asc', async(() => {
+    debugElement.nativeElement.click();
+    fixture.detectChanges();
+    expect(debugElement.query(By.css('i.caret-up'))).toBeTruthy();
   }));
   
-  it('should emit sortingOrPaging event with payload and toggle sortingOrPaging order when clicking the host element repeatedly', async(() => {
-    
-  }));
-  
-  it('should show an upper arrow indicate asc sortingOrPaging order after emit sortingOrPaging event with asc', async(() => {
-    
-  }));
-  
-  it('should show a downward arrow indicate desc sortingOrPaging order after emit sortingOrPaging event with desc', async(() => {
-    
+  it('should show a downward arrow indicate desc order after emit event with desc', async(() => {
+    debugElement.nativeElement.click();
+    fixture.detectChanges();
+    debugElement.nativeElement.click();
+    expect(debugElement.query(By.css('i.caret-down'))).toBeTruthy();
   }));
   
   it('should hide arrow when not ordered by host element\'s property', async(() => {
-    
+    debugElement.nativeElement.click();
+    fixture.detectChanges();
+    const sortByOtherPropertyParam: SortParams = {
+      entity: 'entity',
+      sortBy: 'otherProperty'
+    };
+    tableFilterService.filter(sortByOtherPropertyParam);
+    expect(debugElement.query(By.css('i'))).toBeFalsy();
   }));
   
 });
 
 @Component({
   selector: '',
-  template: '<div ptSortable="propertyName"></div>'
+  template: `<div ptSortable [sortBy]="'property'" [entityName]="'entity'"></div>`
 })
-class TestComponent{}
+class TestComponent {
+}
--- src/app/data-table/sortable/sortable.directive.ts
+++ src/app/data-table/sortable/sortable.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, OnDestroy } from '@angular/core';
+import { TableFilterService, Order } from '../table-filter.service';
+import { Subscription } from 'rxjs';
 
 @Directive({
-  selector: '[ptSortable]'
+  selector: '[ptSortable]',
+  host: {
+    '(click)': 'onClick()'
+  }
 })
-export class SortableDirective {
-
-  constructor() { }
-
+export class SortableDirective implements OnDestroy {
+  ngOnDestroy(): void {
+    if (this._subscription) {
+      this._subscription.unsubscribe();
+    }
+  }
+  
+  @Input() entityName: string;
+  @Input() sortBy: string;
+  order: Order = 'asc';
+  private _subscription: Subscription;
+  
+  constructor(private filterService: TableFilterService) {
+    filterService.filters.subscribe(filters => {
+      
+    })
+  }
+  
+  onClick() {
+    let filterParam = {
+      entity: this.entityName,
+      sortBy: this.sortBy,
+      order: this.order
+    };
+    this.filterService.filter(filterParam);
+    
+    if (this.order === 'asc') {
+      this.order = 'desc';
+    } else if (this.order === 'desc') {
+      this.order = 'asc';
+    }
+  }
 }
