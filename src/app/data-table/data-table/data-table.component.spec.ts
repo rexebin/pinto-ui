e314cf011de8c21ad051ecbd27c2db067c49ffe3
Rex YE <rexebin@gmail.com>
2016-12-20 15:32:22 +0000

    data-table

--- src/app/data-table/data-table/data-table.component.spec.ts
+++ src/app/data-table/data-table/data-table.component.spec.ts
@@ -5,11 +5,12 @@ import { DebugElement } from '@angular/core';
 
 import { DataTableComponent } from './data-table.component';
 import { SearchComponent } from '../../form/search/search.component';
+import { TableFilterService } from '../table-filter.service';
 
 describe('DataTableComponent', () => {
   let component: DataTableComponent;
   let fixture: ComponentFixture<DataTableComponent>;
-
+  let tableService: TableFilterService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DataTableComponent ]
@@ -20,24 +21,17 @@ describe('DataTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DataTableComponent);
     component = fixture.componentInstance;
+    component.entityName = 'entity';
+    tableService = fixture.debugElement.injector.get(TableFilterService);
+    spyOn(tableService, 'init');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  
-  it('should handle search and update items observable', () => {
-    
-    
-    
+  it('should init TableFilterService on init.', () => {
+    expect(tableService.init).toHaveBeenCalledWith('entity');
   });
   
-  it('should listen to TableFilterService and update items observable', () => {
-    
-  });
-  
-  it('should ', () => {
-    
-  });
 });
